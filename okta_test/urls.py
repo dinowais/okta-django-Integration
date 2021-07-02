880156b19fcf2140eeb609b64265ebabef38e2c4
owais mushtaq <owais@trialx.com>
2021-07-02 10:20:33 +0530

    first commit

---
+++ okta_test/urls.py
@@ -0,0 +1,42 @@
+"""okta_test URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from django.urls import include
+from django.conf.urls import url
+import django_saml2_auth.views
+
+urlpatterns = [
+    path('admin/logout/', django_saml2_auth.views.signout),
+    path('admin/', admin.site.urls),
+    # path('', include('django_saml2_auth.urls')),
+    # These are the SAML2 related URLs. You can change "^saml2_auth/" regex to
+    # any path you want, like "^sso_auth/", "^sso_login/", etc. (required)
+    url(r'^saml2_auth/', include('django_saml2_auth.urls')),
+
+    # The following line will replace the default user login with SAML2 (optional)
+    # If you want to specific the after-login-redirect-URL, use parameter "?next=/the/path/you/want"
+    # with this view.
+    url(r'^logout/$', django_saml2_auth.views.signout),
+    url(r'^accounts/login/$', django_saml2_auth.views.signin),
+    url(r'^accounts/logout/$', django_saml2_auth.views.signout),
+    url(r'', django_saml2_auth.views.signin),
+
+    # The following line will replace the admin login with SAML2 (optional)
+    # If you want to specific the after-login-redirect-URL, use parameter "?next=/the/path/you/want"
+    # with this view.
+    url(r'^admin/login/$', django_saml2_auth.views.signin),
+]
